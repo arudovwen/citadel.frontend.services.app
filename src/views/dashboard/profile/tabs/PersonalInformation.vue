f93e2be165a1858cb07dac2b7797a06f5b53ed8b
KaineBismarck <kaine.bismarck@suprbiz.io>
2023-10-12 16:35:16 +0100

    updated personal information fields

--- src/views/dashboard/profile/tabs/PersonalInformation.vue
+++ src/views/dashboard/profile/tabs/PersonalInformation.vue
@@ -2,6 +2,34 @@
   <form @submit.prevent="onSubmit" class="space-y-4">
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
       <Textinput
+        label="First Name"
+        type="text"
+        placeholder="Type your first name"
+        name="firstName"
+        v-model="firstName"
+        :error="firstNameError"
+        classInput="h-[48px]"
+      />
+      <Textinput
+        label="Last Name"
+        type="text"
+        placeholder="Type your last name"
+        name="lastName"
+        v-model="lastName"
+        :error="lastNameError"
+        classInput="h-[48px]"
+      />
+      <Textinput
+        label="Middle Name"
+        type="text"
+        placeholder="Type your middle name"
+        name="middleName"
+        v-model="middleName"
+        :error="middleNameError"
+        classInput="h-[48px]"
+      />
+
+      <Textinput
         label="Email"
         type="email"
         placeholder="Type your email"
@@ -11,20 +39,80 @@
         classInput="h-[48px]"
       />
       <Textinput
-        label="Password"
-        type="password"
-        placeholder="8+ characters, 1 capitat letter "
-        name="password"
-        v-model="password"
-        :error="passwordError"
-        hasicon
+        label="Mobile 1"
+        type="text"
+        placeholder="Type your mobile 1"
+        name="mobile1"
+        v-model="mobile1"
+        :error="mobile1Error"
+        classInput="h-[48px]"
+      />
+
+      <Textinput
+        label="Mobile 2"
+        type="text"
+        placeholder="Type your mobile 2"
+        name="mobile2"
+        v-model="mobile2"
+        :error="mobile2Error"
+        classInput="h-[48px]"
+      />
+      <Textinput
+        label="Address 1"
+        type="text"
+        placeholder="Type your adress 1"
+        name="address1"
+        v-model="address1"
+        :error="address1Error"
+        classInput="h-[48px]"
+      />
+      <Textinput
+        label="Address 2"
+        type="text"
+        placeholder="Type your adress 2"
+        name="address2"
+        v-model="address2"
+        :error="address2Error"
+        classInput="h-[48px]"
+      />
+
+      <Textinput
+        label="Title"
+        type="text"
+        placeholder="Type your title"
+        name="title"
+        v-model="title"
+        :error="titleError"
+        classInput="h-[48px]"
+      />
+
+      <Textinput
+        label="Nearest Bus Stop"
+        type="text"
+        placeholder="Type your nearest bus stop"
+        name="nearestBusStop"
+        v-model="nearestBusStop"
+        :error="nearestBusStopError"
+        classInput="h-[48px]"
+      />
+
+      <Textinput
+        label="Place Of Birth"
+        type="text"
+        placeholder="Type your placeOfBirth"
+        name="placeOfBirth"
+        v-model="placeOfBirth"
+        :error="placeOfBirthError"
         classInput="h-[48px]"
       />
     </div>
 
-    <button type="submit" class="btn btn-dark block w-full text-center">
-      Sign in
-    </button>
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 pt-6">
+      <div class="hidden sm:block"></div>
+      <button type="submit" class="btn btn-dark block w-full text-center">
+        Save Changes
+      </button>
+    </div>
   </form>
 </template>
 
@@ -37,8 +125,25 @@ import { useRouter } from "vue-router";
 
 // Define a validation schema
 const schema = yup.object({
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string().required("Last name is required"),
+  middleName: yup.string(),
   email: yup.string().required("Email is required").email(),
-  password: yup.string().required("Password is required").min(8),
+  mobile1: yup.string(),
+  mobile2: yup.string(),
+  address1: yup.string(),
+  address2: yup.string(),
+  title: yup.string(),
+  nearestBusStop: yup.string(),
+  LGA: yup.string(),
+  state: yup.string(),
+  country: yup.string(),
+  gender: yup.string(),
+  employmentStatus: yup.string(),
+  placeOfBirth: yup.string(),
+  nationality: yup.string(),
+  stateOfOrigin: yup.string(),
+  maritalStatus: yup.string(),
 });
 
 const router = useRouter();
@@ -48,8 +153,25 @@ const goToProfile = () => {
 };
 
 const formValues = {
+  firstName: "",
+  lastName: "",
+  middleName: "",
   email: "dashcode@gmail.com",
-  password: "dashcode",
+  address1: "",
+  address2: "",
+  title: "",
+  mobile1: "",
+  mobile2: "",
+  nearestBusStop: "",
+  LGA: "",
+  state: "",
+  country: "",
+  gender: "",
+  employmentStatus: "",
+  placeOfBirth: "",
+  nationality: "",
+  stateOfOrigin: "",
+  maritalStatus: "",
 };
 
 const { handleSubmit } = useForm({
@@ -58,11 +180,36 @@ const { handleSubmit } = useForm({
 });
 // No need to define rules for fields
 
+const { value: firstName, errorMessage: firstNameError } =
+  useField("firstName");
+const { value: lastName, errorMessage: lastNameError } = useField("lastName");
+const { value: middleName, errorMessage: middleNameError } =
+  useField("middleName");
 const { value: email, errorMessage: emailError } = useField("email");
-const { value: password, errorMessage: passwordError } = useField("password");
+const { value: mobile1, errorMessage: mobile1Error } = useField("mobile1");
+const { value: mobile2, errorMessage: mobile2Error } = useField("mobile2");
+const { value: address1, errorMessage: address1Error } = useField("address1");
+const { value: address2, errorMessage: address2Error } = useField("address2");
+const { value: title, errorMessage: titleError } = useField("title");
+const { value: nearestBusStop, errorMessage: nearestBusStopError } =
+  useField("nearestBusStop");
+const { value: LGA, errorMessage: LGAError } = useField("LGA");
+const { value: state, errorMessage: stateError } = useField("state");
+const { value: country, errorMessage: countryError } = useField("country");
+const { value: gender, errorMessage: genderError } = useField("gender");
+const { value: employmentStatus, errorMessage: employmentStatusError } =
+  useField("employmentStatus");
+const { value: placeOfBirth, errorMessage: placeOfBirthError } =
+  useField("placeOfBirth");
+const { value: nationality, errorMessage: nationalityError } =
+  useField("nationality");
+const { value: stateOfOrigin, errorMessage: stateOfOriginError } =
+  useField("stateOfOrigin");
+
+// const { value: email, errorMessage: emailError } = useField("email");
 
 const onSubmit = handleSubmit((values) => {
-  console.log("PersonalDetails: " + values);
+  console.log("PersonalDetails: " + JSON.stringify(values));
   goToProfile();
 });
 </script>
