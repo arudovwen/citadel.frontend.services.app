85d70e427a4cdb9c0f8b6e38a47cb22dfe5e55b3
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2023-10-12 11:27:15 +0100

    Update Signup.vue

--- src/views/auth/common/Signup.vue
+++ src/views/auth/common/Signup.vue
@@ -47,31 +47,40 @@
       classInput="h-[48px]"
     />
 
-    <label class="cursor-pointer flex items-start">
-      <input
-        type="checkbox"
-        class="hidden"
-        @change="() => (checkbox = !checkbox)"
-      />
-      <span
-        class="h-4 w-4 border rounded inline-flex mr-3 relative flex-none top-1 transition-all duration-150"
-        :class="
-          checkbox
-            ? 'ring-2 ring-black-500 dark:ring-offset-slate-600 dark:ring-slate-900  dark:bg-slate-900 ring-offset-2 bg-slate-900'
-            : 'bg-slate-100 dark:bg-slate-600 border-slate-100 dark:border-slate-600 '
-        "
-      >
-        <img
-          src="@/assets/images/icon/ck-white.svg"
-          alt=""
-          class="h-[10px] w-[10px] block m-auto"
-          v-if="checkbox"
+    <div class="flex items-start">
+      <label class="cursor-pointer flex items-start">
+        <input
+          type="checkbox"
+          class="hidden"
+          @change="() => (checkbox = !checkbox)"
         />
-      </span>
+        <span
+          class="h-4 w-4 border rounded inline-flex mr-3 relative flex-none top-1 transition-all duration-150"
+          :class="
+            checkbox
+              ? 'ring-2 ring-black-500 dark:ring-offset-slate-600 dark:ring-slate-900  dark:bg-slate-900 ring-offset-2 bg-slate-900'
+              : 'bg-slate-100 dark:bg-slate-600 border-slate-100 dark:border-slate-600 '
+          "
+        >
+          <img
+            src="@/assets/images/icon/ck-white.svg"
+            alt=""
+            class="h-[10px] w-[10px] block m-auto"
+            v-if="checkbox"
+          />
+        </span>
+      </label>
       <span class="text-slate-500 dark:text-slate-400 text-sm leading-6"
-        >You accept our Terms and Conditions and Privacy Policy</span
+        >You accept our
+        <router-link class="font-medium" to="/terms"
+          >Terms and Conditions</router-link
+        >
+        and
+        <router-link class="font-medium" to="/privacy"
+          >Privacy Policy</router-link
+        ></span
       >
-    </label>
+    </div>
 
     <button
       type="submit"
