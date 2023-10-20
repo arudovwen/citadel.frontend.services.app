8798f84bdc4b3007b4f979f8fc8c3bf33afaba80
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2023-10-20 14:45:36 +0100

    Update

--- src/components/Pages/Dashboard/Outreach/module/table.vue
+++ src/components/Pages/Dashboard/Outreach/module/table.vue
@@ -297,16 +297,20 @@ export default {
       ],
       options: [
         {
-          value: "1",
-          label: "1",
+          value: "25",
+          label: "25",
         },
         {
-          value: "3",
-          label: "3",
+          value: "50",
+          label: "50",
         },
         {
-          value: "5",
-          label: "5",
+          value: "75",
+          label: "75",
+        },
+        {
+          value: "100",
+          label: "100",
         },
       ],
       columns: [
