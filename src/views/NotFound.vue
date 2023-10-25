a743415fe8570bdd23467251e9b0c37290f5631e
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2023-10-25 22:50:11 +0100

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,15 +1,30 @@
 <template>
   <section class="min-h-screen flex flex-col py-10 px-2 bg-[#E7EBEE] gap-y-2">
     <div class="flex-1 flex flex-col gap-y-3 bg-white rounded-lg">
-      <div class="p-10 text-center flex items-center justify-center">
+      <div class="p-10 text-center flex items-center justify-center mt-20">
         <div>
-          <!-- <img
-            src="@/assets/img/nofound.svg"
-            class="w-[180px] h-auto mx-auto mb-7"
-          /> -->
-
-          <h1 class="text-4xl md:text-8xl font-bold mb-2">404 ERROR!</h1>
-          <h2 class="text-xl font-bold text-gray-600 mb-8">PAGE NOT FOUND</h2>
+          <div class="mobile-logo text-center mb-6 4xl:hidden block">
+            <router-link to="/">
+              <img
+                src="@/assets/images/logo/icon.png"
+                alt=""
+                class="mx-auto w-[100px]"
+                v-if="!this.$store.state.isDark"
+              />
+              <img
+                src="@/assets/images/logo/icon.png"
+                alt=""
+                class="mx-auto w-[100px]"
+                v-else
+              />
+            </router-link>
+          </div>
+          <h1
+            class="text-4xl md:text-8xl font-bold mb-2 text-[rgb(206_154_34)]"
+          >
+            404 <span class="text-gray-800">ERROR!</span>
+          </h1>
+          <h2 class="text-xl font-bold text-gray-800 mb-8">PAGE NOT FOUND</h2>
           <router-link to="/">
             <button
               class="text-white btn-dark px-12 py-4 rounded-full uppercase"
