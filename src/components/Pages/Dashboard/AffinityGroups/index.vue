83f2f5e01e6150ae93b6a5fb52f1d0e2da5e9c48
KaineBismarck <kaine.bismarck@suprbiz.io>
2023-11-03 16:45:13 +0100

    created affinity groups page

---
+++ src/components/Pages/Dashboard/AffinityGroups/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <AffinityGroups />
+  </div>
+</template>
+<script>
+import AffinityGroups from "./module/affinityGroups-table";
+export default {
+  components: {
+    AffinityGroups,
+  },
+};
+</script>
+<style lang=""></style>
