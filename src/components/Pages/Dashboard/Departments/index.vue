e37bcf7b8c2c22728f590124329c8aa6a59875d9
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2023-11-17 19:28:21 +0100

    Update index.vue

--- src/components/Pages/Dashboard/Departments/index.vue
+++ src/components/Pages/Dashboard/Departments/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <div class="flex justify-between items-center mb-4">
-      <div class="flex gap-x-4 items-center">
+    <div
+      class="flex flex-col md:flex-row gap-y-4 md:gap-y-0 md:justify-between md:items-center mb-6 md:mb-4 w-full md:w-auto"
+    >
+      <div
+        class="flex flex-col md:flex-row gap-y-4 md:gap-y-0 md:gap-x-4 items-center w-full md:w-auto"
+      >
         <InputGroup
           v-model="query.searchParameter"
           placeholder="Search department "
