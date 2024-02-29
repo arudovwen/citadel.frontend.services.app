d603c50972c802c5ca5205366fdb0fea947eca15
TOLULOPE <tolu.oduro@appquest.com.ng>
2024-02-29 10:44:53 +0100

    Added Outreach table and create outreach -- fixes

--- src/components/Pages/Dashboard/Outreach/module/table.vue
+++ src/components/Pages/Dashboard/Outreach/module/table.vue
@@ -21,44 +21,29 @@
         </div>
       </div>
       <div class="-mx-6">
-        <vue-good-table :columns="columns" :isLoading="outreachListLoading" mode="remote" styleClass=" vgt-table  centered "
-          :rows="outreachs" :sort-options="{
+        <vue-good-table :columns="columns" :isLoading="outreachListLoading" mode="remote"
+          styleClass=" vgt-table  centered " :rows="outreachs" :sort-options="{
             enabled: false,
           }" :pagination-options="{
   enabled: true,
   perPage: query.pageSize,
 }">
           <template v-slot:table-row="props">
-            <span v-if="props.column.field == 'customer'" class="flex items-center">
-              <span class="w-7 h-7 rounded-full mr-3 flex-none">
-                <img :src="require('@/assets/images/all-img/' +
-                  props.row.customer.image)
-                  " :alt="props.row.customer.name" class="object-cover w-full h-full rounded-full" />
-              </span>
-              <span class="text-sm text-slate-600 dark:text-slate-300 capitalize font-medium">{{ props.row.customer.name
-              }}</span>
+            <span v-if="props.column.field == 'outreachName'" class="font-medium lowercase">
+              {{ props.row.outreachName }}
             </span>
-            <span v-if="props.column.field == 'order'" class="font-medium">
-              {{ "#" + props.row.order }}
+            <span v-if="props.column.field == 'status'" class="font-medium lowercase">
+              <div v-if="props.row.status === 'approved'">
+                approved
+              </div>
+              <div v-if="props.row.status === 'rejected'">
+                rejected
+              </div>
+              <div v-if="!props.row.status">
+                pending
+              </div>
             </span>
-            <span v-if="props.column.field == 'date'" class="text-slate-500 dark:text-slate-400">
-              {{ props.row.date }}
-            </span>
-            <span v-if="props.column.field == 'status'" class="block w-full">
-              <span class="inline-block px-3 min-w-[90px] text-center mx-auto py-1 rounded-[999px] bg-opacity-25" :class="`${props.row.status === 'active'
-                ? 'text-success-500 bg-success-500'
-                : ''
-                } 
-                                                                                                                                                                                                                                                                        ${props.row.status === 'inactive'
-                  ? 'text-warning-500 bg-warning-500'
-                  : ''
-                }
-                                                                                                                                                                                                                                                                        ${props.row.status === 'pending' ? 'text-blue-500 bg-blue-500' : ''}
             
-             `">
-                {{ props.row.status }}
-              </span>
-            </span>
             <span v-if="props.column.field == 'action'">
               <Dropdown classMenuItems=" w-[140px]">
                 <span class="text-xl">
@@ -87,8 +72,8 @@
           <template #pagination-bottom="props">
             <div class="py-4 px-3">
               <Pagination :total="total" :current="query.pageNumber" :per-page="query.pageSize" :pageRange="pageRange"
-                @page-changed="query.pageNumber = $event" :perPageChanged="perPage" enableSearch
-                enableSelect :options="options">
+                @page-changed="query.pageNumber = $event" :perPageChanged="perPage" enableSearch enableSelect
+                :options="options">
                 >
               </Pagination>
             </div>
@@ -208,6 +193,22 @@ export default {
         },
         {
           name: "delete",
+          icon: "heroicons-outline:trash",
+          doit: (data) => {
+            this.type = data.name;
+            this.$refs.modal.openModal();
+          },
+        },
+        {
+          name: "approve",
+          icon: "heroicons-outline:trash",
+          doit: (data) => {
+            this.type = data.name;
+            this.$refs.modal.openModal();
+          },
+        },
+        {
+          name: "reject",
           icon: "heroicons-outline:trash",
           doit: (data) => {
             this.type = data.name;
