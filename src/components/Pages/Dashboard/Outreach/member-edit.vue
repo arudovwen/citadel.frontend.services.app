99b68e2f8bc99ea6cc8def48d418f0fadb4ee2e0
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-03-04 11:42:13 +0100

    Update member-edit.vue

--- src/components/Pages/Dashboard/Outreach/member-edit.vue
+++ src/components/Pages/Dashboard/Outreach/member-edit.vue
@@ -3,23 +3,52 @@
     <Card title="">
       <div class="grid gap-5">
         <div class="">
-          <Textinput label="Name" v-model="name" :error="nameError" placeholder="Provide name for outreach" />
+          <Textinput
+            label="Name"
+            v-model="outreachName"
+            :error="outreachNameError"
+            placeholder="Provide outreachName for outreach"
+          />
         </div>
-        <FormGroup label="Date of outreach" name="dateOfOutreach" :error="dateOfOutreachError">
-          <flat-pickr v-model="dateOfOutreach" class="form-control" id="d1" placeholder="yyyy, dd M" />
+        <FormGroup
+          label="Date of outreach"
+          outreachName="dateOfOutreach"
+          :error="dateOfOutreachError"
+        >
+          <flat-pickr
+            v-model="dateOfOutreach"
+            class="form-control"
+            id="d1"
+            placeholder="yyyy, dd M"
+          />
         </FormGroup>
 
         <div class="">
-          <Textinput label="Location" v-model="location" :error="locationError"
-            placeholder="Provide location for outreach" />
+          <Textinput
+            label="Location"
+            v-model="locationOfOutreach"
+            :error="locationOfOutreachError"
+            placeholder="Provide locationOfOutreach for outreach"
+          />
         </div>
 
-        <Textarea label="Description" type="text" :rows="4" v-model="description" :error="descriptionError"
-          placeholder="Provide a description" />
+        <Textarea
+          label="Description"
+          type="text"
+          :rows="4"
+          v-model="description"
+          :error="descriptionError"
+          placeholder="Provide a description"
+        />
       </div>
 
       <div class="text-right space-x-3 mt-8">
-        <Button :isLoading="state.profile.editOutreachRequestLoading" type="submit" text="Submit" btnClass="btn-dark" />
+        <Button
+          :isLoading="state.profile.editOutreachRequestLoading"
+          type="submit"
+          text="Submit"
+          btnClass="btn-dark"
+        />
       </div>
     </Card>
   </form>
@@ -35,28 +64,28 @@ import FormGroup from "@/components/FormGroup";
 import Textinput from "@/components/Textinput";
 import Textarea from "@/components/Textarea";
 import { useStore } from "vuex";
-import moment from "moment"
+// import moment from "moment";
 
 const { state, dispatch } = useStore();
 
 const formData = reactive({
   dateOfOutreach: "",
   description: "",
-  location: "",
-  name: "",
+  locationOfOutreach: "",
+  outreachName: "",
 });
 const formDataSchema = yup.object().shape({
-  name: yup
+  outreachName: yup
     .string()
     .typeError("Please enter a valid date")
-    .required("Please provie a name"),
+    .required("Please provie a outreachName"),
 
   dateOfOutreach: yup
     .date()
     .typeError("Please enter a valid date")
     .required("Date is required"),
 
-  location: yup.string().required("Location is required"),
+  locationOfOutreach: yup.string().required("Location is required"),
   description: yup.string().required("A short description is required"),
 });
 
@@ -67,13 +96,13 @@ const { handleSubmit } = useForm({
   initialValues:
     {
       ...props.data,
-      name: props.data.outreachName,
-      location: props.data.locationOfOutreach,
     } || formData,
 });
 
-const { value: name, errorMessage: nameError } = useField("name");
-const { value: location, errorMessage: locationError } = useField("location");
+const { value: outreachName, errorMessage: outreachNameError } =
+  useField("outreachName");
+const { value: locationOfOutreach, errorMessage: locationOfOutreachError } =
+  useField("locationOfOutreach");
 const { value: description, errorMessage: descriptionError } =
   useField("description");
 
@@ -83,11 +112,6 @@ const { value: dateOfOutreach, errorMessage: dateOfOutreachError } =
 const onSubmit = handleSubmit((values) => {
   const data = {
     ...values,
-    outreachName: values.name,
-    dateOfOutreach: moment(values.date).format(
-      "YYYY-MM-DDTHH:mm:ss.SSSZ"
-    ),
-    locationOfOutreach: values.location
   };
   dispatch("editOutreachRequest", data);
 });
