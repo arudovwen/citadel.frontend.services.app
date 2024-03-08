e67fe478126788c79b63b5966acda0093c24fa4c
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-03-08 12:47:58 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -198,7 +198,7 @@ const routes = [
         name: "accessories",
         component: AccessoriesIndex,
         meta: {
-          roles: "CAN_CREATE_VENUES",
+          roles: "CAN_VIEW_ACCESSORIES",
           subroles: [],
         },
         children: [
